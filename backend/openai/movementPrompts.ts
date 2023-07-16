28655aca8fbf4a7d42543774c92c1efce9f74290
Justin <justintorre75@gmail.com>
2023-07-15 17:27:45 -0700

    BE in working state

--- backend/openai/movementPrompts.ts
+++ backend/openai/movementPrompts.ts
@@ -1,12 +1,14 @@
 // @ts-ignore
 import { CallOpenAIProps, OpenAIResponse } from "./index.ts";
 // @ts-ignore
-import { Action, AgentProfile } from "../backendTypes.ts";
+import { Action, AgentProfile, Interaction } from "../backendTypes.ts";
 
 export function getMoveDirectionPrompt(
   boardDimensions: [number, number],
   agentPosition: [number, number],
-  nearbyAgents: AgentProfile[]
+  nearbyAgents: AgentProfile[],
+  previousInteractions: Interaction[],
+  interactionsTowardsMe: Interaction[]
 ): CallOpenAIProps {
   const [width, height] = boardDimensions;
   const [x, y] = agentPosition;
@@ -40,6 +42,32 @@ You see ${
     } other agents nearby. You can move in any direction or talk to any other.
 Here are all the Agents near you and their name:
 ${nearbyAgents.map((a) => `${a.name}: ${a.id}`).join("\n")}
+
+
+${previousInteractions.length > 0 ? "Here are your previous interactions:" : ""}
+${previousInteractions
+  .map((i) => {
+    if (i.action.action._typename === "Interact") {
+      return `You interacted with ${i.action.agentId} and said ${i.action.action.message.text}`;
+    } else {
+      return `You moved ${i.action.action.direction}`;
+    }
+  })
+  .join("\n")}
+
+${
+  interactionsTowardsMe.length > 0
+    ? "Here are interactions other agents have made towards you:"
+    : ""
+}
+${interactionsTowardsMe
+  .map((i) => {
+    if (i.action.action._typename === "Interact") {
+      return `${i.action.agentId} interacted with you and said ${i.action.action.message.text}`;
+    }
+  })
+  .join("\n")}
+
 `,
     // TODO add a list of their previous actions
     user: `
@@ -73,20 +101,41 @@ Dont forget to be friendly to the other agents!
               description: "The ID of the agent to interact with",
               enum: agentIds,
             },
+            chatText: {
+              type: "string",
+              description: "What you want to say to the neighboring agent",
+              enum: agentIds,
+            },
           },
-          required: ["agentId"],
+          required: ["agentId", "chatText"],
         },
       },
     ],
   };
 }
 
-export function getAction(response: OpenAIResponse): Action {
-  // TODO
-  return {
-    type: "move",
-    payload: {
-      direction: "up",
-    },
-  };
+export function getAction(response: OpenAIResponse):
+  | Action
+  | {
+      error: string;
+    } {
+  const fnCall = response.choices[0]?.message?.function_call;
+  if (fnCall?.name === "move") {
+    return {
+      _typename: "Move",
+      direction: fnCall.arguments.direction,
+    };
+  } else if (fnCall?.name === "interact") {
+    return {
+      _typename: "Interact",
+      destionationAgentId: fnCall.arguments.agentId,
+      message: {
+        text: fnCall.arguments.chatText,
+      },
+    };
+  } else {
+    return {
+      error: "Unknown function call" + JSON.stringify(fnCall),
+    };
+  }
 }
--- src/components/IslandBody.tsx
+++ src/components/IslandBody.tsx
@@ -2,38 +2,39 @@
 
 import { useWebsocket } from "@/hooks/useWebhooks";
 import Image from "next/image";
-import { useState } from "react";
-import dynamic from 'next/dynamic';
+import { useEffect, useState } from "react";
+import dynamic from "next/dynamic";
 
-const IslandMap:any = dynamic(() => import('../components/IslandMap'), {
+const IslandMap: any = dynamic(() => import("../components/IslandMap"), {
   ssr: false,
 });
 
 export function IslandBody() {
-  const { sendMessage, subscribe } = useWebsocket(
-    "ws://localhost:8080"
-  );
+  const { sendMessage, subscribe } = useWebsocket("ws://localhost:8080");
   const [message, setMessage] = useState("");
+  useEffect(() => {
+    subscribe((message) => {
+      console.log("message", message);
+    });
+  }, []);
   return (
     <div className="">
-      <div className="">
-        <div className="z-30 w-full max-w-5xl items-center justify-center text-center text-xl lg:flex text-betterBlack mb-10">
-          Love Island
-        </div>
-        <textarea
-          className="z-30 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex text-black bg-white"
-          onChange={(e) => {
-            sendMessage(e.target.value);
-            setMessage(e.target.value);
-          }}
-          value={message}
-        ></textarea>
-      </div>
-      <div className="">
-        <div className="pt-5 min-h-[600px] bg-primary flex justify-center">
-          <div id="hexagon-board-parent" className="min-w-[800px] min-h-[400px]  bg-primary  rounded-lg  overflow-hidden relative">
-            <IslandMap god={true} subscribe={subscribe}/>
-          </div>
+      {/* response */}
+      <textarea
+        className="z-30 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex text-black bg-white"
+        onChange={(e) => {
+          sendMessage(e.target.value);
+          setMessage(e.target.value);
+        }}
+        value={message}
+      ></textarea>
+
+      <div className="pt-10 min-h-[600px] bg-gray-100 flex justify-center">
+        <div
+          id="hexagon-board-parent"
+          className="min-w-[800px] min-h-[400px] max-w-[90vw] bg-white border border-gray-300 rounded-lg  overflow-hidden relative"
+        >
+          <IslandMap god={true} subscribe={subscribe} />
         </div>
       </div>
     </div>

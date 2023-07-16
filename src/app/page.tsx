4daf4eecca2fc1c07f5021130d5d00d0f59d3ae2
Justin <justintorre75@gmail.com>
2023-07-15 18:31:57 -0700

    logs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { IslandBody } from "@/components/IslandBody";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-
 export default function Home() {
   const queryClient = new QueryClient();
 
@@ -12,7 +11,9 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center content-center justify-between p-24 bg-primary">
         <h2 className="font-serif text-5xl text-betterBlack">💕🤖💕</h2>
         <h1 className="font-serif text-7xl text-betterBlack">Love AIsland</h1>
-        <h2 className="font-serif italic text-3xl text-betterBlack">Would ChatGPT swipe right on <b className="text-rose-500">you</b>?</h2>
+        <h2 className="font-serif italic text-3xl text-betterBlack">
+          Would ChatGPT swipe right on <b className="text-rose-500">you</b>?
+        </h2>
         <IslandBody />
         <div className="mb-32 mt-32 justify-center text-center lg:mb-0 lg:grid-cols-4 bg-primary text-betterBlack">
           AIs need love @2023
